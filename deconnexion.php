9fc7378303aaf4da8a8f04e54d934f3f0edd4d51
isaure <isaure.vico@student.junia.com>
2022-05-06 19:37:02 +0200

    page déco

--- deconnexion.php
+++ deconnexion.php
@@ -1,8 +1,50 @@
-<?php
-    //mettre le code de deconnexion
-    session_start();
-    session_unset();
-    session_destroy();
-    header("Location:connexion.php");
+<!DOCTYPE html>
+<html>
+<body>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="menu_deroulant.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="connexion.css" media="screen" type="text/css" />
+
+    </head>
     
-?>
+    <body background="images/photoInte.jpg" bgproperties="fixed">
+        <div class="back"></div>
+
+        <div id="container">
+           <form method="POST">
+                   <h1>Déconnexion</h1>
+
+                   <br>
+
+                   <label><i><b>Êtes-vous sûr de vouloir vous déconnecter ?</b></i></label>
+
+                   <br>
+                   <br>
+
+                   <input type="submit" id='submitConnect' name="submitConnect" value='Se Déconnecter'>
+
+                  <i> <p  class="inscription"> Vous ne souhaitez pas vous déconnecter ? </i><br>
+                   <a href="menu.php"> Retourner sur l'album photo</a></p>
+               </form>
+                
+            <?php
+                //mettre le code de deconnexion
+                session_start();
+                if(isset($_POST['submitConnect'])){
+                    $choix = $_POST["choix"];
+                    if($choix == "oui"){
+                        session_unset();
+                        session_destroy();
+                        header("Location:connexion.php");
+                    }
+                }
+                
+
+            ?>
+        </div>
+        
+    </body>
+</html>
+
