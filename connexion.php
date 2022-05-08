53a69ef6e67b8d21abbde8275e2a8078fd953594
isaure <isaure.vico@student.junia.com>
2022-05-08 14:20:55 +0200

    commentaires ajoutés sur les 2 pages de co.php

--- connexion.php
+++ connexion.php
@@ -11,61 +11,63 @@
     
     <body background="images/photoInte.jpg" bgproperties="fixed">
         <div class="back"></div>
-        <!-- <nav class="menuD"> 
-            <a href="menu.php">Profil</a>
-            <a href="add.php">Add new pic</a>
-            <a href="deconnexion.php">Déconnexion</a>
-        </nav> -->
         <div id="container">
-            <form action="connexionBIS.php" method="POST">
+            
+            <!-- Formulaire de connexion -->
+            <form action="connexionBIS.php" method="POST">  <!-- ConnexionBis.php : page de formulaire associée -->
                 <h1>Connexion</h1>
 
                 <label><b>Nom d'utilisateur</b></label>
+                
                 <?php 
                     if(isset($_COOKIE['username'])){ 
                         $var=$_COOKIE['username'];
                 ?>
+
                 <input type="text" value="<?php echo $var;?>" id="username" name="username" required>
+                
                 <?php
                     }
                     else{
                 ?>
+
                 <input type="text" placeholder="nom_profil" id="username" name="username" required>
+               
                 <?php } ?>
 
 
                 <label><b>Email</b></label>
+                
                 <?php 
                     if(isset($_COOKIE['mail'])){ 
                         $var=$_COOKIE['mail'];
                 ?>
+
                 <input type="mail" value="<?php echo $var?>" id="mail" name="mail" required>
+                
                 <?php
                     }
                     else{
                 ?>
-                <input type="mail" placeholder="instogram@email.com" id="mail" name="mail" required>
-                <?php } ?>
 
+                <input type="mail" placeholder="instogram@email.com" id="mail" name="mail" required>
+                
+                <?php } ?>
 
                 <label><b>Mot de passe</b></label>
                 <input type="password" placeholder="votre_mot_de_passe" id="password" name="password" required>
 
                 <a href="#" class="forgotMDP">Mot de passe oublié ?</a>
-
-                <!--Il faut set le cookie-->
                 <input type="checkbox" value="souvenir" name="souvenir">
                 <label for="souvenir">Se souvenir de moi</label>
+
 
                 <input type="submit" id='submitConnect' name="submitConnect" value='LOGIN'>
 
                 <p class="inscription">Pas encore inscrit ? <a href="create.php">Créer un compte</a></p>
-            </form>
-                
-            
-        </div>
-        
-    </body>
 
+            </form>
+        </div>
+    </body>
 </body>
 </html>
--- connexionBIS.php
+++ connexionBIS.php
@@ -1,34 +1,45 @@
 <?php
     session_start();
     if(isset($_POST['submitConnect'])){
+        //on vérifie maintenant si le champ "username" est vide
         if(empty($_POST['username'])){
             echo "Le champ username est vide.";
         } else {
-            // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
+            // on vérifie maintenant si le champ "Mail" est vide
             if(empty($_POST['mail'])){
                 echo "Le champ Mail est vide.";
             } else {
+                // on vérifie si le champ "Mot de passe" est vide
                 if(empty($_POST['password'])){
                     echo "Le champ password est vide.";
+
+                    //si aucun de ses champs n'est vide, on se connect à la base de données
                 } else {
                     $username = htmlentities($_POST['username'], ENT_QUOTES, "UTF-8"); 
                     $mdp = htmlentities($_POST['password'], ENT_QUOTES, "UTF-8");
                     $mail = htmlentities($_POST['mail'], ENT_QUOTES, "UTF-8");
 
                     //connexion DB :
-                    $nameDB="ProjetWeb"; //Instogram pour Isaure et ProjetWeb pour Alex
+                    $nameDB="ProjetWeb"; //Nom de la base de données
                     $connexion = mysqli_connect("localhost","root","root","ProjetWeb");
-
-                    if(!$connexion){
+                    
+                    // on vérifie si la connexion a échouée
+                    if(!$connexion){ 
                         echo"<h1>Erreur de connexion".mysqli_connect_error()."</h1>";
                         die();
                     }
+
+                    // Si la connexion est réussite, on initialise la requête de connexion
                     $requete_connect="SELECT * FROM users WHERE pseudo_profil='".$username."' AND mdp_profil='".$mdp."' AND email_profil='".$mail."'";
                     $result_connect=mysqli_query($connexion, $requete_connect);
+
+                    // on vérifie le résultat de cette requête
                     if(mysqli_num_rows($result_connect)==0){
                         echo "<p>L'une des valeurs entrées est incorrecte</p>";
                         echo '<br> <a href="connexion.php"> Retour page de connexion </a>';
                     }
+
+                    // on initialise les cookies 
                     else{
                         $_SESSION['pseudo'] = $username;
                         if(isset($_POST['souvenir'])){
